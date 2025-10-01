fe162b184cbb6dba609e400d1360940e302fc44a
alejanosorio <alejoo870@gmail.com>
2025-09-30 21:18:00 -0300

    string

---
+++ estructuraDatos/Strings.java
@@ -0,0 +1,11 @@
+package estructuraDatos;
+
+public class Strings {
+
+    public static void main(String[] args) {
+        String nombre ="alex";
+
+        System.out.println("el nombre empieza con : "+ nombre.charAt(0) + "  cantidad de letras : "+nombre.length());
+
+    }
+}
